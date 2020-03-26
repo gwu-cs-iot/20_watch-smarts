7ec7cb1d263ae0eaf6adc40819d3dfc5a0a62f05
Sam Hanna <samanthahanna15@gwu.edu>
2020-03-26 12:51:37 -0400

    basic website beginnings

---
+++ website/Exercises.js
@@ -0,0 +1,17 @@
+class Exercises{
+  constructor(){
+
+  }
+
+  breathing(){
+    window.alert("Breathing test")
+  }
+
+  mindful(){
+    window.alert("Mindful test")
+  }
+
+  focus(){
+    window.alert("Focus test")
+  }
+}
